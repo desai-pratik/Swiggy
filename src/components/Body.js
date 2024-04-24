0e423fd811268f8c522183768df2c4005a7fd051
desai-pratik <pratikdesai2274@gmail.com>
2024-04-24 16:22:20 +0530

    cors-anywhere

--- src/components/Body.js
+++ src/components/Body.js
@@ -17,7 +17,7 @@ const Body = () => {
 
   const fechData = async () => { 
     const data = await fetch(
-      "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.18880&lng=72.82930&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.18880&lng=72.82930&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     setlistfrestorent(
